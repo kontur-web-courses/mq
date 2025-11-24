acd2465e7898c3cd163497b6a51cf6e33ec11278
Alexey404Ivanov <AA.Ivanov2@urfu.me>
2025-11-24 15:55:58 +0500

    task 2-3

--- producer/Program.cs
+++ producer/Program.cs
@@ -1,11 +1,12 @@
 using MassTransit;
+using producer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddScoped<MyPublisher>();
 builder.Services.AddMassTransit(config =>
 {
     config.UsingRabbitMq((context, cfg) => { 
@@ -30,8 +31,9 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", async (MyPublisher publisher) =>
 {
+    await publisher.Publish("Weather endpoint was called");
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
