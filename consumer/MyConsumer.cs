fd8fd1383d7191af89595c96c40384cd9a899a95
futig <everymusic8@gmail.com>
2025-11-24 15:44:01 +0500

    3

---
+++ consumer/MyConsumer.cs
@@ -0,0 +1,13 @@
+using MassTransit;
+using Models;
+
+namespace Consumer;
+
+public class MyConsumer : IConsumer<MyMessage>
+{
+    public Task Consume(ConsumeContext<MyMessage> context)
+    {
+        Console.WriteLine($"[{context.Message.MessageId}] {context.Message.Content}");
+        return Task.CompletedTask;
+    }
+}
