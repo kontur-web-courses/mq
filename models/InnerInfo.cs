7ed791773216f3daf9624591159c384ec0b004bb
Konstantin Kolesnikov <kolesnikov.kons@gmail.com>
2025-11-24 15:38:19 +0500

    fix: 1 feat: 2

---
+++ models/InnerInfo.cs
@@ -0,0 +1,6 @@
+namespace models;
+
+public class InnerInfo
+{
+    public string Info { get; set; }
+}
--- producer/Program.cs
+++ producer/Program.cs
@@ -1,8 +1,22 @@
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((context, cfg) => { 
+        cfg.Host("localhost", "/", h => { 
+            h.Username("guest");
+            h.Password("guest");
+        });
+    });
+});
+
+builder.Services.AddScoped<MyPublisher>();
 
 var app = builder.Build();
 
@@ -19,7 +33,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", async (MyPublisher publisher) =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
@@ -29,6 +43,8 @@ app.MapGet("/weatherforecast", () =>
             summaries[Random.Shared.Next(summaries.Length)]
         ))
         .ToArray();
+    
+    await publisher.Publish(string.Join(",", forecast.Select(f => f.Summary)));
     return forecast;
 })
 .WithName("GetWeatherForecast");
