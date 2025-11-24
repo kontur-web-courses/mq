944a6fb1a0af8fb912c0a693e01e8aca426d9867
Iesht <irinashteynfeld@gmail.com>
2025-11-24 16:12:01 +0500

    all done

---
+++ consumer/ForecastConsumer.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using Models;
+
+namespace consumer;
+
+public class ForecastConsumer : IConsumer<DailyWeatherForecast>
+{
+    public Task Consume(ConsumeContext<DailyWeatherForecast> context)
+    {
+        Console.WriteLine("CONSUMER");
+        Console.WriteLine($"[{context.Message.Date}] temp={context.Message.TemperatureC} | {context.Message.TemperatureF} deg\nsummary={context.Message.Summary}");
+        return Task.CompletedTask;
+    }
+}
---
+++ producer/ForcastPublisher.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using Models;
+
+namespace producer;
+
+public class ForcastPublisher
+{
+    private readonly ISendEndpointProvider _sendEndpointProvider;
+
+    public ForcastPublisher(ISendEndpointProvider sendEndpointProvider)
+    {
+        _sendEndpointProvider = sendEndpointProvider;
+    }
+
+    public async Task Publish(DailyWeatherForecast forecast)
+    {
+        var publishEndpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri("queue:forecast-queue"));
+        await publishEndpoint.Send(forecast);
+    }
+}
