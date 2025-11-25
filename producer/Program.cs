423c2f7df83abc10454d002667328bdcf5d834bf
den-ibr <ibr.den.dan@gmail.com>
2025-11-25 15:38:45 +0500

    2, 3

--- producer/Program.cs
+++ producer/Program.cs
@@ -1,8 +1,23 @@
+using MassTransit;
+using Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<MyPublisher>();
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+    });
+});
 
 var app = builder.Build();
 
@@ -19,9 +34,10 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+app.MapGet("/weatherforecast", async (MyPublisher publisher) =>
+    {
+        await publisher.Publish("AAA");
+        var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -38,4 +54,20 @@ app.Run();
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class MyPublisher
+{
+    private readonly ISendEndpointProvider _sendEndpointProvider;
+
+    public MyPublisher(ISendEndpointProvider sendEndpointProvider)
+    {
+        _sendEndpointProvider = sendEndpointProvider;
+    }
+
+    public async Task Publish(string content)
+    {
+        var publishEndpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri("queue:my-queue"));
+        await publishEndpoint.Send(new Human{ Name = content, IQ = Random.Shared.Next(70, 130) });
+    }
 }
