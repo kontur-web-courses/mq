3100527ec682525b82ea4fb74eaa7af7e8349ad4
ArtemToporkov <toporkovartem259@gmail.com>
2025-11-25 11:16:29 +0500

    add messages publisher

---
+++ producer/publishers/MyPublisher.cs
@@ -0,0 +1,13 @@
+﻿using MassTransit;
+using Models;
+
+namespace producer.publishers;
+
+public class MyPublisher(ISendEndpointProvider sendEndpointProvider)
+{
+    public async Task Publish(string content)
+    {
+        var publishEndpoint = await sendEndpointProvider.GetSendEndpoint(new Uri("queue:my-queue"));
+        await publishEndpoint.Send(new MyMessage { MessageId = Guid.NewGuid(), Content = content });
+    }
+}
