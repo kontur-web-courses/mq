f0268a4954bd96131323bc3a5454a20ccc7a6116
antoshkaxxr <gbanton2004@yandex.ru>
2025-11-25 15:21:08 +0500

    task2 done

---
+++ Models/OrderMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Models;
+
+public class OrderMessage
+{
+    public Guid OrderId { get; set; }
+    public string CustomerName { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
