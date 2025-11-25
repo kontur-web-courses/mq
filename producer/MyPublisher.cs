97363d755c5a866cc49dc4923a1f9eea01073981
ERDAS <com123465@mail.ru>
2025-11-25 16:01:23 +0500

    fix

--- producer/MyPublisher.cs
+++ producer/MyPublisher.cs
@@ -1,6 +1,13 @@
+using MassTransit;
+using Models;
+
 namespace producer;
 
-public class MyPublisher
+public class MyPublisher(IPublishEndpoint  publishEndpoint)
 {
-    
+    public async Task Publish(string content)
+    {
+        //var publishEndpoint = await sendEndpointProvider.GetSendEndpoint(new Uri("queue:my-queue"));
+        await publishEndpoint.Publish(new MyMessage { MessageId = Guid.NewGuid(), Content = content });
+    }
 }
