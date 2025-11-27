cf156e14b147202a16ca8f20cc45e0bb00ab77a2
IIIMAXIMIII <145893187+IIIMAXIMIII@users.noreply.github.com>
2025-11-27 11:52:06 +0500

    готово

--- producer/Program.cs
+++ producer/Program.cs
@@ -1,8 +1,20 @@
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((context, cfg) => {
+        cfg.Host("localhost", "/", h => {
+            h.Username("guest");
+            h.Password("guest");
+        });
+    });
+});
+builder.Services.AddScoped<MyPublisher>();
 
 var app = builder.Build();
 
@@ -19,8 +31,10 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", async (MyPublisher publisher) =>
 {
+    await publisher.Publish($"Weather forecast requested at {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
+
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
