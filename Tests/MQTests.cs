1961c82a636d75ce2c83779c8c3a4c8072e7a041
strannikhead <144272555+strannikhead@users.noreply.github.com>
2025-11-27 18:04:57 +0500

    Add tests

---
+++ Tests/MQTests.cs
@@ -0,0 +1,40 @@
+﻿using consumer;
+using MassTransit;
+using MassTransit.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Models;
+
+namespace Tests;
+
+public class MQTests
+{
+    [Test]
+    public async Task MyConsumer_should_consume_MyMessage()
+    {
+        await using var provider = new ServiceCollection()
+            .AddMassTransitTestHarness(cfg => { cfg.AddConsumer<MyConsumer>(); })
+            .BuildServiceProvider(validateScopes: true);
+
+        var harness = provider.GetTestHarness();
+
+        await harness.Start();
+
+        try
+        {
+            await harness.Bus.Publish(new MyMessage
+            {
+                MessageId = Guid.NewGuid(),
+                Content = "hello from test"
+            });
+
+            Assert.That(await harness.Consumed.Any<MyMessage>());
+
+            var consumerHarness = harness.GetConsumerHarness<MyConsumer>();
+            Assert.That(await consumerHarness.Consumed.Any<MyMessage>());
+        }
+        finally
+        {
+            await harness.Stop();
+        }
+    }
+}
