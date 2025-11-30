6405cbd35c340aa2287ea5b0710ffa180f70263a
JohnyCeleron <147262524+JohnyCeleron@users.noreply.github.com>
2025-11-30 18:23:49 +0500

    2

--- producer/Program.cs
+++ producer/Program.cs
@@ -1,9 +1,21 @@
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((context, cfg) => { 
+        cfg.Host("localhost", "/", h => { 
+            h.Username("guest");
+            h.Password("guest");
+        });
+    });
+});
+builder.Services.AddScoped<MyPublisher>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,7 +31,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", async ([FromServices] MyPublisher publisher) =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
@@ -29,6 +41,8 @@ app.MapGet("/weatherforecast", () =>
             summaries[Random.Shared.Next(summaries.Length)]
         ))
         .ToArray();
+
+    await publisher.Publish(string.Join(",", forecast.Select(f => f.Summary)));
     return forecast;
 })
 .WithName("GetWeatherForecast");
