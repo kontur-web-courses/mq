a6da2b41bc210beb08f434311aa154ee250f08cf
ISich <arseny.voroni@yandex.ru>
2025-12-19 00:08:00 +0500

    done

---
+++ consumer/MyConsumer.cs
@@ -0,0 +1,11 @@
+using MassTransit;
+using Models;
+
+public class MyConsumer: IConsumer<MyMessage>
+{
+    public Task Consume(ConsumeContext<MyMessage> context)
+    {
+        Console.WriteLine($"[{context.Message.MessageId}] {context.Message.Content}");
+        return Task.CompletedTask;
+    }
+}
